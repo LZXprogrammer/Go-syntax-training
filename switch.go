8e9efc6e487c09f2eadbe79522e93ffb2a8236ed
LZXprogrammer <lzxprogrammer@outlook.com>
2018-05-08 16:52:53 +0800

    练习 switch

--- switch.go
+++ switch.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	// "runtime"
+	"time"
 )
 
 func main() {
-	fmt.Print("Go runs on ")
+	// fmt.Print("Go runs on ")
 
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux.")
+	// switch os := runtime.GOOS; os {
+	// case "darwin":
+	// 	fmt.Println("OS X.")
+	// case "linux":
+	// 	fmt.Println("Linux.")
+	// default:
+	// 	fmt.Println("s%.", os)
+	// }
+
+	/*fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today")
+	case today + 1:
+		fmt.Println("Tomorrow")
+	case today + 2:
+		fmt.Println("In two days")
+	case today + 3:
+		fmt.Println("In three days")
+	case today + 4:
+		fmt.Println("In four days")
 	default:
-		fmt.Println("s%.", os)
+		fmt.Println("Too far away")
+	}
+	*/
+
+	// 没有条件的 switch
+	t := time.Now()
+	switch {
+	case t.Hour() > 12:
+		fmt.Println("Good morning")
+	case t.Hour() > 0 && t.Hour() < 12:
+		fmt.Println("Good afternoon")
+	default:
+		fmt.Println("Good evening")
 	}
 }
